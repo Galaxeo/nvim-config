f149450b08d055d8f31a8ae367c6ac0ffd77a193
Justin Cheok <hwjustincheok@gmail.com>
2024-10-07 14:21:42 -0700

    feat: add back coderunner

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,12 @@ function map(mode, lhs, rhs, opts)
     options = vim.tbl_extend("force", options, opts)
   end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { noremap = true, silent = false })
+  vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false })
+  vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false })
+  vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false })
+  vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false })
+  vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false })
+  vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false })
 end
 map("i", "jk", "<ESC>")
